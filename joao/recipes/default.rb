15a0bc499ef328cbbf4c15882e7daeedc8bdbd9c
João Carvalho <oi@joaocarvalhowd.com.br>
2017-01-06 17:22:05 -0200

    test

--- joao/recipes/default.rb
+++ joao/recipes/default.rb
@@ -1,1 +1,9 @@
-include_recipe "apache2"
+execute "timezone" do
+    command "unlink /etc/localtime && ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime && date"
+    action :run
+end
+
+package "nginx" do
+    source "#{Chef::Config[:file_cache_path]}/nginx-1.8.0-1.el6.ngx.eotica.x86_64.rpm"
+    action :install
+end
